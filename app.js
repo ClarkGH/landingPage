c16a71d2d2c2183ee5825adb4ba220b8f9fb3e84
clarkgh <demolisho@hotmail.com>
2018-02-22 20:22:00 -0800

    fixing port for heroku

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ var express = require('express'),
   app = express(),
   path = require('path');
 
+app.set('port', (process.env.PORT || 5000));
+
 app.use(express.static('./dist/'))
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-app.listen(8080, () => {
-  console.log('App is now listening on port 8080!');
+app.listen('port', () => {
+  console.log('Node app is now listening on port: ', app.get('port'));
 });
