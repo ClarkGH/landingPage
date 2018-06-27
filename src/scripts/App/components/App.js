33bb6db20d34ad12b7fbe67b10c3719fecdf056b
clarkgh <demolisho@hotmail.com>
2018-06-26 19:38:55 -0700

    removing home component for now, unnecessary

--- src/scripts/App/components/App.js
+++ src/scripts/App/components/App.js
@@ -1,13 +1,53 @@
 import React, { Component } from 'react';
-import Home from './Home';
+import { Route, Link } from "react-router-dom";
+import About from './About';
+import Portfolio from './Portfolio';
 
-
-class App extends Component {
-  render() {
-    return (
-      <Home />
-    );
-  }
+const HomePage = () => {
+  return (<div>
+    <div className="split left">
+      <h1>About Clark</h1>
+      <Link to="/portfolio" className="button">Read More</Link>
+    </div>
+    <div className="split right">
+      <h1>Projects</h1>
+      <a href="#" className="button">Read More</a>
+    </div>
+  </div>);
 }
 
-export default App;
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  componentDidMount() {
+    const left = document.querySelector('.left');
+    const right = document.querySelector('.right');
+    const container = document.querySelector('.container');
+
+    left.addEventListener('mouseenter', () => {
+      container.classList.add('hover-left');
+    });
+
+    left.addEventListener('mouseleave', () => {
+      container.classList.remove('hover-left');
+    });
+
+    right.addEventListener('mouseenter', () => {
+      container.classList.add('hover-right');
+    });
+
+    right.addEventListener('mouseleave', () => {
+      container.classList.remove('hover-right');
+    });
+  }
+
+  render() {
+    return <div>
+      <Route path="/" exact component={HomePage} />
+      <Route path="/about" component={About} />
+      <Route path="/portfolio" component={Portfolio} />
+    </div>
+  }
+}
